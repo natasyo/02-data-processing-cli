f99f16607262a97775024fe36d1fee9ef6d00019
Natallia <natasyo23@gmail.com>
2026-03-14 10:58:59 +0300

    feat: csv to json

---
+++ src/commands/csvToJson.js
@@ -0,0 +1,59 @@
+import fs from "fs/promises"
+import {createReadStream, writeFile} from "fs"
+import path from "path";
+import {Readable, Transform, Writable, pipeline} from "stream";
+export async function csvToJson(currentPath,  args) {
+    if(args.length!==4) throw new Error("Error: csvToJson command argument format: --input data.csv --output data.json");
+    const inputIndex=args.indexOf('--input');
+    const outputIndex=args.indexOf('--output');
+    if(inputIndex===-1||outputIndex===-1){
+        throw new Error("Error: csvToJson command argument format: --input data.csv --output data.json");
+    }
+    const inputFile=path.join(currentPath, args[inputIndex+1]);
+    const outputFile=path.join(currentPath,args[outputIndex+1]);
+    try{
+        await fs.access(inputFile, fs.constants.R_OK);
+        const readable=createReadStream(inputFile, 'utf8');
+        let text="";
+        const transform=new Transform({
+        transform(chunk, encoding, callback) {
+        text+=chunk;
+            const rows=text.split('\n');
+        text=rows.pop();
+
+        const keys=rows[0].split(',')
+        const items= rows.slice(1).map((row)=>{
+            let obj={};
+            row.split(',').forEach((line, index)=>{
+                obj[keys[index]] = line;
+            })
+            return obj;
+        })
+               callback(null, JSON.stringify(items, null, 2));
+            }
+        });
+        const writable=new Writable({
+          write(chunk, encoding, callback) {
+              console.log(chunk, encoding);
+              try{
+                 writeFile(outputFile,chunk,async ()=>{
+                  await   fs.writeFile(outputFile,chunk)
+                 });
+                  callback(null, chunk);
+              }
+              catch(err){
+                  console.error(err);
+              }
+            }
+        })
+        pipeline(readable, transform, writable, (err, result) => {
+            if(err) { console.error(err);
+            if(result){ console.log('result',result); }
+            }
+        });
+    }
+    catch(error){
+        throw error;
+    }
+
+}
--- src/repl.js
+++ src/repl.js
@@ -1,6 +1,7 @@
 import { stdin, stdout } from "process";
 import readline from "readline";
 import {cd, ls, up} from "./navigation.js";
+import {csvToJson} from "./commands/csvToJson.js";
 
 function parseCommandLine(line){
   const [command, ...args]=line.split(" ");
@@ -17,23 +18,32 @@ export async function repl() {
   });
   rl.prompt();
   rl.on("line", async (input) => {
-    const {command, args} = parseCommandLine(input);
-    parseCommandLine(input);
-    switch (command) {
-      case "up":
-        currentPath = up(currentPath);
-            break;
-      case "cd":
-        currentPath =await cd(currentPath, args);
-        break;
-      case "ls":
-       await ls(currentPath);
-        break;
-      default:
+    try{
+      const {command, args} = parseCommandLine(input);
+      parseCommandLine(input);
+      switch (command) {
+        case "up":
+          currentPath = up(currentPath);
+          break;
+        case "cd":
+          currentPath =await cd(currentPath, args);
+          break;
+        case "ls":
+          await ls(currentPath);
+          break;
+        case "csv-to-json":
+          await  csvToJson(currentPath, args);
+          break;
+        default:
           console.log("Unknown command: " + command);
           break;
+      }
+      rl.prompt();
+    } catch (error) {
+      console.error(error.message);
+      rl.prompt();
     }
-    rl.prompt();
+
   });
 
   rl.on("close", () => {
